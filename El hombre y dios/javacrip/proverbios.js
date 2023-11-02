5bdf2a63f124061fde36db71194c33db397d0029
Elhombreydios23 <149440718+Elhombreydios23@users.noreply.github.com>
2023-11-02 13:41:12 -0400

    Delete El hombre y dios directory

--- El hombre y dios/javacrip/proverbios.js
+++
@@ -1,5 +0,0 @@
-var curiosidades2 = document.getElementById('curiosidades2');
-    
-function scrollcuriosidades(direction) {
-    curiosidades2.scrollLeft += direction * curiosidades2.offsetWidth * .3;
-}
